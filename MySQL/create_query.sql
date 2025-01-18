341523447811c23ce6e7a77b69be1ff4f12f4b71
Ankush Singh <91226946+KushAnku@users.noreply.github.com>
2025-01-17 22:03:54 -0500

    Update create_query.sql

--- MySQL/create_query.sql
+++ MySQL/create_query.sql
@@ -1,1 +1,10 @@
 
+SELECT * FROM db_book;
+SELECT * FROM db_customer;
+SELECT * FROM db_employee;
+SELECT * FROM db_order;
+SELECT * FROM db_order_detail;
+SELECT * FROM db_shipper;
+SELECT * FROM db_subject;
+SELECT * FROM db_supplier;
+
